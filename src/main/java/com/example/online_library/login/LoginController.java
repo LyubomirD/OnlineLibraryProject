8ec4349fdb92576e7971478f3685d0d91c03da34
Lyubo <lubodimoff@gmail.com>
2023-12-07 18:58:19 +0200

    UI retrieves cookie SessionID, LoginController return cookie sessionId LoginController logic should be in LoginService

--- src/main/java/com/example/online_library/login/LoginController.java
+++ src/main/java/com/example/online_library/login/LoginController.java
@@ -14,6 +14,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.Collections;
 import java.util.UUID;
 
 @RestController
@@ -23,10 +24,10 @@ public class LoginController {
 
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
     private final LoginService loginService;
-    private static final int SESSION_DURATION_SECONDS = 300; // 5 minutes
+    private static final int SESSION_DURATION_SECONDS = 24 * 60 * 60; // 24 hours / 300 = 5 min
 
     @PostMapping
-    public ResponseEntity<String> login(@RequestHeader("Authorization") String authHeader,
+    public ResponseEntity<?> login(@RequestHeader("Authorization") String authHeader,
                                    HttpServletResponse response) {
         String[] credentials = getCredentials(authHeader);
 
@@ -44,13 +45,15 @@ public class LoginController {
                 sessionCookie.setMaxAge(SESSION_DURATION_SECONDS);
                 sessionCookie.setHttpOnly(true);
                 sessionCookie.setPath("/");
+                sessionCookie.setDomain("localhost");
 
                 log.info("Setting cookie: {}={}", sessionCookie.getName(), sessionCookie.getValue());
                 log.info("Cookie session: {}", sessionCookie);
 
                 response.addCookie(sessionCookie);
 
-                return ResponseEntity.ok("{\"message\":\"Login successful\"}");
+
+                return ResponseEntity.ok(Collections.singletonMap("sessionId", sessionId));
             }
         }
 
--- src/main/java/com/example/online_library/web_security/config/ConfigCORS.java
+++ src/main/java/com/example/online_library/web_security/config/ConfigCORS.java
@@ -16,11 +16,17 @@ public class ConfigCORS {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+        corsConfiguration.setAllowedHeaders(Arrays.asList(
+                "Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-                "Access-Control-Request-Method", "Access-Control-Request-Headers"));
-        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-                "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+                "Access-Control-Request-Method", "Access-Control-Request-Headers",
+                "Set-Cookie"
+        ));
+        corsConfiguration.setExposedHeaders(Arrays.asList(
+                "Origin", "Content-Type", "Accept", "Authorization",
+                "Access-Control-Allow-Origin", "Access-Control-Allow-Origin",
+                "Access-Control-Allow-Credentials", "Set-Cookie"
+        ));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
