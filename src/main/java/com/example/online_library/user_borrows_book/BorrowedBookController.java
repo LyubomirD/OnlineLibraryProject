49bd573b0877ce2fd7a38c9e85e74f9b41844ca9
Lyubo <lubodimoff@gmail.com>
2023-12-07 13:41:42 +0200

    User can login using Basic Auth, each user has a session cookie, Only users that are ADMIN can add, update, delete books thanks to their cookie, Users can borrow, remove and view their books, each user is independent thanks to the my_session_id cookie that keeps track of the email and userRole

--- src/main/java/com/example/online_library/user_borrows_book/BorrowedBookController.java
+++ src/main/java/com/example/online_library/user_borrows_book/BorrowedBookController.java
@@ -5,6 +5,7 @@ import com.example.online_library.models.book.Book;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 import java.util.Set;
 
@@ -16,17 +17,17 @@ public class BorrowedBookController {
     private final BorrowedBookService borrowedBookService;
 
     @GetMapping("/view")
-    public Set<Book> viewUserBook() {
-        return borrowedBookService.viewAllUsersBook();
+    public Set<Book> viewUserBook(HttpServletRequest httpServletRequest) {
+        return borrowedBookService.viewAllUsersBook(httpServletRequest);
     }
 
     @PostMapping("/borrow")
-    public Optional<AppUser> borrowBooks(@RequestBody BorrowedBookRequest request) {
-        return borrowedBookService.addBookToUser(request);
+    public Optional<AppUser> borrowBooks(@RequestBody BorrowedBookRequest request, HttpServletRequest httpServletRequest) {
+        return borrowedBookService.addBookToUser(request, httpServletRequest);
     }
 
     @PutMapping("/remove")
-    public Optional<AppUser> removeBorrowedBook(@RequestBody BorrowedBookRequest request) {
-        return borrowedBookService.removeBookFromUser(request);
+    public Optional<AppUser> removeBorrowedBook(@RequestBody BorrowedBookRequest request, HttpServletRequest httpServletRequest) {
+        return borrowedBookService.removeBookFromUser(request, httpServletRequest);
     }
 }
--- src/main/java/com/example/online_library/user_borrows_book/BorrowedBookService.java
+++ src/main/java/com/example/online_library/user_borrows_book/BorrowedBookService.java
@@ -6,10 +6,10 @@ import com.example.online_library.models.appuser.UserService;
 import com.example.online_library.models.book.Book;
 import com.example.online_library.models.book.BookAdminService;
 import lombok.AllArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 import java.util.Set;
 
@@ -20,25 +20,42 @@ public class BorrowedBookService {
     private final BookAdminService bookAdminService;
     private final UserService userService;
 
-    public Set<Book> viewAllUsersBook() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    private String getEmailFromCookie(HttpServletRequest httpServletRequest) {
+        Cookie[] cookies = httpServletRequest.getCookies();
+        String email = null;
 
-        if (authentication == null) {
-            throw new EmailValidationException("Email is not validated of not registered");
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("MY_SESSION_ID".equals(cookie.getName())) {
+                    String[] cookieParts = cookie.getValue().split(":");
+                    if (cookieParts.length > 1) {
+                        email = cookieParts[1];
+                        break;
+                    }
+                }
+            }
         }
-        String email = authentication.getName();
+
+        return email;
+    }
+
+    public Set<Book> viewAllUsersBook(HttpServletRequest httpServletRequest) {
+        String email = getEmailFromCookie(httpServletRequest);
+
+        if (email == null) {
+            throw new EmailValidationException("Email is not validated or not registered");
+        }
+
         Optional<AppUser> user = userService.findUserByEmail(email);
 
         return user.get().getBooks();
     }
 
-    private Optional<AppUser> modifyBookAction(BorrowedBookRequest request, boolean addBook) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    private Optional<AppUser> modifyBookAction(BorrowedBookRequest request, boolean addBook, HttpServletRequest httpServletRequest) {
+        String email = getEmailFromCookie(httpServletRequest);
 
-        if (authentication != null) {
-            String email = authentication.getName();
+        if (email != null) {
             Optional<AppUser> userOptional = userService.findUserByEmail(email);
-
             String title = request.getTitle();
             String author = request.getAuthor();
             Optional<Book> bookOptional = bookAdminService.findBookByTitleAndAuthor(title, author);
@@ -62,12 +79,11 @@ public class BorrowedBookService {
         return Optional.empty();
     }
 
-    public Optional<AppUser> addBookToUser(BorrowedBookRequest request) {
-        return modifyBookAction(request, true);
+    public Optional<AppUser> addBookToUser(BorrowedBookRequest request, HttpServletRequest httpServletRequest) {
+        return modifyBookAction(request, true, httpServletRequest);
     }
 
-    public Optional<AppUser> removeBookFromUser(BorrowedBookRequest request) {
-        return modifyBookAction(request, false);
+    public Optional<AppUser> removeBookFromUser(BorrowedBookRequest request, HttpServletRequest httpServletRequest) {
+        return modifyBookAction(request, false, httpServletRequest);
     }
-
 }
