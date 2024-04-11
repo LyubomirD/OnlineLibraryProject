cc8f254b375844e2ecfb199ec95d982233692118
Lyubo <lubodimoff@gmail.com>
2024-04-11 23:19:22 +0300

    Can add book with jwt token

--- src/main/java/com/example/online_library/library/admin/LibraryAdminController.java
+++ src/main/java/com/example/online_library/library/admin/LibraryAdminController.java
@@ -1,34 +1,54 @@
 package com.example.online_library.library.admin;
 
 import com.example.online_library.mapper.dto.LibraryRequestDto;
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
+import com.example.online_library.models.jwt_token.JwtService;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping(path = "api/v1/library-admin")
 @AllArgsConstructor
 public class LibraryAdminController {
 
     private final LibraryAdminService libraryAdminService;
+    private final JwtService jwtService;
 
-    @GetMapping("get-bookId/{title}/{author}")
-    public Long getBookById(@PathVariable String title, @PathVariable String author, HttpServletRequest httpServletRequest) {
-        return libraryAdminService.getBookId(title, author, httpServletRequest);
-    }
+
+//    @GetMapping("get-bookId/{title}/{author}")
+//    public Long getBookById(@PathVariable String title, @PathVariable String author, HttpServletRequest httpServletRequest) {
+//        return libraryAdminService.getBookId(title, author, httpServletRequest);
+//    }
 
     @PostMapping("add-book")
-    public void includeNewBookToLibrary(@RequestBody LibraryRequestDto request, HttpServletRequest httpServletRequest) {
-        libraryAdminService.includeNewBookToLibrary(request, httpServletRequest);
+    public ResponseEntity<?> includeNewBookToLibrary(@RequestBody LibraryRequestDto request, HttpServletRequest httpServletRequest) {
+        // Get JWT token from request headers
+        String token = extractTokenFromRequest(httpServletRequest);
+        // Call service method with token
+        return libraryAdminService.includeNewBookToLibrary(request, token);
     }
 
-    @PutMapping("update-book/{book_id}")
-    public void updateBookInformation(@PathVariable Long book_id, @RequestBody LibraryRequestDto request, HttpServletRequest httpServletRequest) {
-        libraryAdminService.changeExistingBookInform(book_id, request, httpServletRequest);
-    }
+//    @PutMapping("update-book/{book_id}")
+//    public void updateBookInformation(@PathVariable Long book_id, @RequestBody LibraryRequestDto request, HttpServletRequest httpServletRequest) {
+//        libraryAdminService.changeExistingBookInform(book_id, request, httpServletRequest);
+//    }
+//
+//    @DeleteMapping("delete-book/{book_id}")
+//    public void deleteBookFromLibrary(@PathVariable Long book_id, HttpServletRequest httpServletRequest) {
+//        libraryAdminService.deleteBookFromLibrary(book_id, httpServletRequest);
+//    }
 
-    @DeleteMapping("delete-book/{book_id}")
-    public void deleteBookFromLibrary(@PathVariable Long book_id, HttpServletRequest httpServletRequest) {
-        libraryAdminService.deleteBookFromLibrary(book_id, httpServletRequest);
+    private String extractTokenFromRequest(HttpServletRequest request) {
+        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (token != null && token.startsWith("Bearer ")) {
+            return token.substring(7);
+        } else {
+            throw new RuntimeException("JWT token not found in request headers");
+        }
     }
 }
--- src/main/java/com/example/online_library/library/admin/LibraryAdminService.java
+++ src/main/java/com/example/online_library/library/admin/LibraryAdminService.java
@@ -1,20 +1,16 @@
 package com.example.online_library.library.admin;
 
 import com.example.online_library.exceptions.AdminAccessDeniedException;
-import com.example.online_library.login.encryptUserSession.EncryptionUtils;
 import com.example.online_library.mapper.dto.LibraryRequestDto;
 import com.example.online_library.mapper.mappers.LibraryRequestMapper;
 import com.example.online_library.models.appuser.UserRole;
 import com.example.online_library.models.appuser.UserService;
 import com.example.online_library.models.book.Book;
 import com.example.online_library.models.book.BookAdminService;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
+import com.example.online_library.models.jwt_token.JwtService;
 import lombok.AllArgsConstructor;
-import org.hibernate.SessionException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import javax.crypto.SecretKey;
 
 
 @Service
@@ -24,106 +20,56 @@ public class LibraryAdminService {
     private final BookAdminService bookAdminService;
     private final UserService userService;
     private final LibraryRequestMapper libraryRequestMapper;
-    private static final String SESSION_NAME = "MY_SESSION_ID";
+    private final JwtService jwtService;
     private static final UserRole ADMIN = UserRole.ADMIN;
 
-
-    private void checkSessionForUserExistingAndAdminRole(HttpServletRequest httpServletRequest) throws RuntimeException {
-        Cookie[] cookies = httpServletRequest.getCookies();
-        System.out.println("Cookies[]: " + cookies);
-
-        if (cookies == null) {
-            throw new SessionException("Session cookie not found");
-        }
-
-        for (Cookie cookie : cookies) {
-            if (isSessionCookie(cookie)) {
-                handleSessionCookie(cookie);
-            }
+    private void checkUserIsAdmin(String token) {
+        String username = jwtService.extractUsername(token);
+        if (!userService.findUserByEmailAndRole(username, ADMIN)) {
+            throw new AdminAccessDeniedException("User is not an administrator");
         }
     }
 
-    private boolean isSessionCookie(Cookie cookie) {
-        return SESSION_NAME.equals(cookie.getName());
+//    public Long getBookId(String title, String author, HttpServletRequest httpServletRequest) {
+//
+//        try {
+//        checkSessionForUserExistingAndAdminRole(httpServletRequest);
+//
+//        return bookAdminService.findBookId(title, author);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            throw new RuntimeException("User does not have permission to add a book", e);
+//        }
+//    }
+
+    public ResponseEntity<?> includeNewBookToLibrary(LibraryRequestDto request, String token) {
+        checkUserIsAdmin(token);
+        Book book = libraryRequestMapper.libraryRequestDtoToBook(request);
+        bookAdminService.addNewBook(book);
+        return null;
     }
 
-    private void handleSessionCookie(Cookie cookie) throws RuntimeException{
-        try {
-            SecretKey secretKey = EncryptionUtils.generateSecretKey();
-            String decryptedSessionData = decryptSessionCookie(cookie, secretKey);
-
-            System.out.println("Decrypted session: " + decryptedSessionData);
-
-            String[] sessionParts = decryptedSessionData.split(":");
-            String email = (sessionParts.length > 1) ? sessionParts[1] : null;
-            String role = (sessionParts.length > 2) ? sessionParts[2] : null;
-
-            System.out.println("Email: " + email);
-            System.out.println("Role: " + role);
-
-            if (!ADMIN.equals(UserRole.valueOf(role))) {
-                throw new AdminAccessDeniedException("User is not an administrator");
-            }
-
-            if (!userService.findUserByEmailAndRole(email, ADMIN)) {
-                throw new AdminAccessDeniedException("User not found or not an administrator");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("Error handling session cookie", e);
-        }
-    }
-
-    private String decryptSessionCookie(Cookie cookie, SecretKey secretKey) throws Exception {
-        String encryptedSessionData = cookie.getValue();
-        return EncryptionUtils.decrypt(encryptedSessionData, secretKey);
-    }
-
-
-    public Long getBookId(String title, String author, HttpServletRequest httpServletRequest) {
-
-        try {
-        checkSessionForUserExistingAndAdminRole(httpServletRequest);
-
-        return bookAdminService.findBookId(title, author);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("User does not have permission to add a book", e);
-        }
-    }
-
-    public void includeNewBookToLibrary(LibraryRequestDto request, HttpServletRequest httpServletRequest) {
-        try {
-            checkSessionForUserExistingAndAdminRole(httpServletRequest);
-
-            Book book = libraryRequestMapper.libraryRequestDtoToBook(request);
-            bookAdminService.addNewBook(book);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("User does not have permission to add a book", e);
-        }
-    }
-
-    public void changeExistingBookInform(Long book_id, LibraryRequestDto request, HttpServletRequest httpServletRequest) {
-        try {
-            checkSessionForUserExistingAndAdminRole(httpServletRequest);
-
-            Book book = libraryRequestMapper.libraryRequestDtoToBook(request);
-            bookAdminService.updateExistingBook(book_id, book);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("User does not have permission to add a book", e);
-        }
-    }
-
-    public void deleteBookFromLibrary(Long book_id, HttpServletRequest httpServletRequest) {
-        try {
-            checkSessionForUserExistingAndAdminRole(httpServletRequest);
-
-            bookAdminService.deleteBook(book_id);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("User does not have permission to add a book", e);
-        }
-    }
+//
+//    public void changeExistingBookInform(Long book_id, LibraryRequestDto request, HttpServletRequest httpServletRequest) {
+//        try {
+//            checkSessionForUserExistingAndAdminRole(httpServletRequest);
+//
+//            Book book = libraryRequestMapper.libraryRequestDtoToBook(request);
+//            bookAdminService.updateExistingBook(book_id, book);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            throw new RuntimeException("User does not have permission to add a book", e);
+//        }
+//    }
+//
+//    public void deleteBookFromLibrary(Long book_id, HttpServletRequest httpServletRequest) {
+//        try {
+//            checkSessionForUserExistingAndAdminRole(httpServletRequest);
+//
+//            bookAdminService.deleteBook(book_id);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            throw new RuntimeException("User does not have permission to add a book", e);
+//        }
+//    }
 }
